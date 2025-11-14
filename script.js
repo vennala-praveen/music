a6e38f514ab70a0c55cea427b32257e505f1da0a
Praveen Vennala <praveen98vennala@gmail.com>
2025-11-14 20:19:45 +0530

    added js file

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Stop all audio
 function stopAllAudio() {
   document.querySelectorAll("audio").forEach(audio => {
     audio.pause();
@@ -5,6 +6,7 @@ function stopAllAudio() {
   });
 }
 
+// Toggle play/pause when clicking image
 function toggleAudio(id) {
   let audio = document.getElementById(id);
 
@@ -15,3 +17,19 @@ function toggleAudio(id) {
     audio.pause();
   }
 }
+
+// Playlist order
+const playlist = ["audio1", "audio2", "audio3", "audio4", "audio5"];
+
+// Attach event listeners for auto-play
+playlist.forEach((id, index) => {
+  let audio = document.getElementById(id);
+  audio.addEventListener("ended", () => {
+    let nextIndex = index + 1;
+    if (nextIndex < playlist.length) {
+      let nextAudio = document.getElementById(playlist[nextIndex]);
+      stopAllAudio();
+      nextAudio.play();
+    }
+  });
+});
