38c60da0f9a4b57ea2aaf5c74a7acdf4dd5c5071
Praveen Vennala <praveen98vennala@gmail.com>
2025-11-14 20:14:17 +0530

    added index&style&js file

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
 function stopAllAudio() {
-    document.querySelectorAll("audio").forEach(audio => audio.pause());
+  document.querySelectorAll("audio").forEach(audio => {
+    audio.pause();
+    audio.currentTime = 0; // reset to start
+  });
 }
-function function1() {
+
+function toggleAudio(id) {
+  let audio = document.getElementById(id);
+
+  if (audio.paused) {
     stopAllAudio();
-    document.getElementById("audio1").play();
-}
-		function function2() {
-    stopAllAudio();
-    document.getElementById("audio2").play();
-}
-		function function3() {
-    stopAllAudio();
-    document.getElementById("audio3").play();
-}
-		function function4() {
-    stopAllAudio();
-    document.getElementById("audio4").play();
-}
-		function function5() {
-    stopAllAudio();
-    document.getElementById("audio5").play();
+    audio.play();
+  } else {
+    audio.pause();
+  }
 }
