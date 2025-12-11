a9ca9129b69d3e40ac3182535145991513cd7be2
Praveen Vennala <praveen98vennala@gmail.com>
2025-12-11 11:51:56 +0530

    commited progressbar file

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function toggleAudio(id) {
 
 // Playlist order
 const playlist = ["audio1", "audio2", "audio3", "audio4", "audio5", "audio6", "audio7", "audio8", 
-                  "audio9", "audio10", "audio11", "audio12", "audio13", "audio14", "audio15", "audio16"];
+                  "audio9", "audio10", "audio11", "audio12", "audio13", "audio14", "audio15", "audio16", "audio17"];
 
 // Attach event listeners for auto-play + progress bar
 playlist.forEach((id, index) => {
